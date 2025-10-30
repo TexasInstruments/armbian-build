95e40358cee9b84e61b8e039d1063ba8f7c03a45
Jonas Wood <jw@ti.com>
2025-10-30 14:59:32 -0500

    ti: DON'T UPSTREAM: Add AM62Lx demo app and overlay OOTB
    Signed-off-by: Jonas Wood <jw@ti.com>

--- config/bootscripts/boot-k3.cmd
+++ config/bootscripts/boot-k3.cmd
@@ -7,3 +7,7 @@ get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
 uenvcmd=run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}; setexpr fdtfile sub ti/ti ti; run bootcmd_ti_mmc
 
 optargs=vt.global_cursor_default=0
+
+if test "${board_name}" = "am62lx"; then
+    name_overlays=ti/k3-am62l3-evm-epaper.dtbo ti/k3-am62l3-evm-m2-cc3351.dtbo
+fi
