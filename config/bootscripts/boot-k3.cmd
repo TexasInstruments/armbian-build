f2ab005f305f833bd8b818ca512581e598321e1d
Jonas Wood <jw@ti.com>
2025-12-09 15:47:58 -0600

    ti: DON'T UPSTREAM: Add AM62L Badge overlay OOTB and TI src to *-next
    Signed-off-by: Jonas Wood <jw@ti.com>

--- config/bootscripts/boot-k3.cmd
+++ config/bootscripts/boot-k3.cmd
@@ -1,9 +1,15 @@
 bootpart=1:1
 bootdir=
 finduuid=part uuid \${boot} 1:2 uuid
-name_rd=uInitrd
-get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
+if test "${fdtfile}" != "ti/k3-am62l3-badge.dtb"; then
+    name_rd=uInitrd
+    get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
+fi
 
 uenvcmd=run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}; setexpr fdtfile sub ti/ti ti; run bootcmd_ti_mmc
 
 optargs=vt.global_cursor_default=0
+
+if test "${fdtfile}" = "ti/k3-am62l3-badge.dtb"; then
+    setenv name_overlays ti/k3-am62l3-evm-eink-gdey042t81.dtbo
+fi
