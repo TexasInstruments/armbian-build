f9bf3f43b7ebf4a6dba0932ab2a111149ce0a011
Jonas Wood <jw@ti.com>
2025-12-17 01:00:37 -0600

    ti: extensions: ti-debpkgs: Use error handling
    Signed-off-by: Jonas Wood <jw@ti.com>

--- extensions/ti-debpkgs.sh
+++ extensions/ti-debpkgs.sh
@@ -59,7 +59,7 @@ function post_install_kernel_debs__activate_dkms() {
 
 function post_customize_image__rm_aptconf() {
     display_alert "Removing apt.conf file"
-    run_host_command_logged "rm ${SDCARD}/etc/apt/apt.conf"
+    run_host_command_logged "rm -f ${SDCARD}/etc/apt/apt.conf"
     chroot_sdcard_apt_get_update || true
     display_alert "Removed apt.conf"
 }
