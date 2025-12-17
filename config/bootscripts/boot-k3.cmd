e95f4abad228d0040edf3208db2a4a8dbaf966aa
Jonas Wood <jw@ti.com>
2025-12-17 14:41:11 -0600

    ti: DON'T UPSTREAM: Change BeagleBadge rdaddr
    Signed-off-by: Jonas Wood <jw@ti.com>

--- config/bootscripts/boot-k3.cmd
+++ config/bootscripts/boot-k3.cmd
@@ -1,15 +1,13 @@
 bootpart=1:1
 bootdir=
 finduuid=part uuid \${boot} 1:2 uuid
-if test "${fdtfile}" != "ti/k3-am62l3-badge.dtb"; then
-    name_rd=uInitrd
-    get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
+if test "${fdtfile}" = "ti/k3-am62l3-badge.dtb"; then
+    rdaddr=0x84900000
+    setenv name_overlays ti/k3-am62l3-evm-eink-gdey042t81.dtbo
 fi
+name_rd=uInitrd
+get_rd_mmc=load mmc ${bootpart} ${rdaddr} ${bootdir}/${name_rd}
 
 uenvcmd=run get_rd_${boot}; env set rd_spec ${rdaddr}:${filesize}; setexpr fdtfile sub ti/ti ti; run bootcmd_ti_mmc
 
 optargs=vt.global_cursor_default=0
-
-if test "${fdtfile}" = "ti/k3-am62l3-badge.dtb"; then
-    setenv name_overlays ti/k3-am62l3-evm-eink-gdey042t81.dtbo
-fi
