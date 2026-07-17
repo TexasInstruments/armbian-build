9445faa8c1210ae6d1f66a5d9a3b4e22be38f5b0
Jonas Wood <jw@ti.com>
2026-07-17 18:59:46 -0500

    ti: extensions: Fix ti-debpkgs OS suite detection
    Previously it used GitHub API to pull ti-debpkgs InRelease to check
    supported OS suite versions. Occasionally that would error due GitHub
    API rate limits.
    
    Signed-off-by: Jonas Wood <jw@ti.com>

--- extensions/ti-debpkgs.sh
+++ extensions/ti-debpkgs.sh
@@ -1,19 +1,14 @@
 function post_repo_customize_image__100_install_ti_packages() {
+	# Check if OS suite is supported by TI debian package repo
+	local http_code=$(curl -s -o /dev/null -w "%{http_code}" https://raw.githubusercontent.com/TexasInstruments/ti-debpkgs/main/dists/${RELEASE}/InRelease)
 
-    # Read JSON array into Bash array safely
-	mapfile -t valid_suites < <(
-		curl -s https://api.github.com/repos/TexasInstruments/ti-debpkgs/contents/dists |
-		jq -r '.[].name'
-	)
-	display_alert "TI Repo has the following valid suites - ${valid_suites[@]}..."
-
-	if printf '%s\n' "${valid_suites[@]}" | grep -qx "${RELEASE}"; then
+	if [[ "${http_code}" == "200" ]]; then
 		# Get the sources file
 		run_host_command_logged "mkdir -p \"$SDCARD/tmp\""
 		run_host_command_logged "wget -qO $SDCARD/tmp/ti-debpkgs.sources https://raw.githubusercontent.com/TexasInstruments/ti-debpkgs/main/ti-debpkgs.sources"
 
 		# Update suite in source file
-		chroot_sdcard "sed -i 's/bookworm/${RELEASE}/g' /tmp/ti-debpkgs.sources"
+		chroot_sdcard "sed -i 's/Suites: .*/Suites: ${RELEASE}/g' /tmp/ti-debpkgs.sources"
 
 		# Copy updated sources file into chroot
 		chroot_sdcard "cp /tmp/ti-debpkgs.sources /etc/apt/sources.list.d/ti-debpkgs.sources"
@@ -40,10 +35,11 @@ function post_repo_customize_image__100_install_ti_packages() {
 			do_with_retries 3 chroot_sdcard_apt_get -o "Dpkg::Options::=--force-overwrite" --no-install-recommends install "${TI_PACKAGES[@]}"
 		fi
 
+	elif [[ "${http_code}" == "404" ]]; then
+		local valid_suites=$(curl -s "https://github.com/TexasInstruments/ti-debpkgs/tree/main/dists" | grep -oP '(?<=/ti-debpkgs/tree/main/dists/)[^"]+' | grep -v "^stable$" | sort -u | tr '\n' ' ')
+		exit_with_error "Detected OS suite ${RELEASE} is not supported by TI debian package repo. Exiting!" "Valid OS suites are: ${valid_suites}"
 	else
-		# Error if suite is not valid but continue building image anyway
-		display_alert "Error: Detected OS suite '$RELEASE' is not valid based on TI package repository. Skipping!"
-		display_alert "Valid Options Would Have Been: ${valid_suites[@]}"
+		exit_with_error "Failed to access TI debian package repo (HTTP code ${http_code}). Exiting!"
 	fi
 }
 
